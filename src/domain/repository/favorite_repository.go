61d2fa8f7895175cc7ddd21193708cfb1cb5647a
xxarupakaxx <arupaka3tdigks@gmail.com>
2021-11-14 02:57:00 +0900

    repoの追加

--- src/domain/repository/favorite_repository.go
+++ src/domain/repository/favorite_repository.go
@@ -1,1 +1,7 @@
 package repository
+
+type IFavoriteRepository interface {
+	FindAll(placeID string) []string
+	Save(id uint, placeID string) bool
+	Delete(id uint, placeID string) bool
+}
--- src/domain/repository/user_repository.go
+++ src/domain/repository/user_repository.go
@@ -1,1 +1,6 @@
 package repository
+
+type IUserRepository interface {
+	Save(lineUserID string) uint
+	FindOne(lineUserID string) uint
+}
