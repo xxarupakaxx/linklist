bfec8e620884898835cc98643d16978af45f662e
xxarupakaxx <arupaka3tdigks@gmail.com>
2021-11-14 19:08:01 +0900

    gorm.ioに

--- go.mod
+++ go.mod
@@ -7,6 +7,11 @@ require (
 	github.com/go-sql-driver/mysql v1.6.0 // indirect
 	github.com/jinzhu/gorm v1.9.16 // indirect
 	github.com/jinzhu/inflection v1.0.0 // indirect
+	github.com/jinzhu/now v1.1.2 // indirect
+	github.com/mattn/go-sqlite3 v1.14.9 // indirect
 	github.com/sirupsen/logrus v1.8.1 // indirect
 	golang.org/x/sys v0.0.0-20200323222414-85ca7c5b95cd // indirect
+	gorm.io/driver/mysql v1.1.3 // indirect
+	gorm.io/driver/sqlite v1.2.4 // indirect
+	gorm.io/gorm v1.22.3 // indirect
 )
--- src/infra/mysql.go
+++ src/infra/mysql.go
@@ -1,20 +1,20 @@
 package infra
 
 import (
-	"github.com/jinzhu/gorm"
-	_ "github.com/jinzhu/gorm/dialects/mysql"
 	"github.com/sirupsen/logrus"
+	"gorm.io/driver/mysql"
+	"gorm.io/gorm"
 	"os"
 )
 
 func DBConnect(db *gorm.DB, err error) {
-	dbDriver := "mysql"
 	dbUser := os.Getenv("DB_USERNAME")
 	dbPass := os.Getenv("DB_PASSWORD")
 	dbName := os.Getenv("DB_NAME")
 	dbHostname := os.Getenv("DB_HOSTNAME")
 	dbOption := "?parseTime=true&loc=Asia/Tokyo"
-	db, err = gorm.Open(dbDriver, dbUser+":"+dbPass+"@tcp("+dbHostname+":3306)/"+dbName+dbOption)
+	dsn := dbUser+":"+dbPass+"@tcp("+dbHostname+":3306)/"+dbName+dbOption
+	db,err = gorm.Open(mysql.Open(dsn),&gorm.Config{})
 	if err != nil {
 		logrus.Fatalf("failed in Connect DB:%w", err)
 	}
